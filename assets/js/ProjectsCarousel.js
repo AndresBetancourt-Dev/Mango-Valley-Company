829e40e03d690d6d5576ca60281e0daaa5b12feb
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-09-19 14:29:31 -0500

    UI Finished in Theory

---
+++ assets/js/ProjectsCarousel.js
@@ -0,0 +1,31 @@
+const projects = document.querySelectorAll(".project-container");
+const projectsCarousel = document.querySelector(".projects");
+const projectsLeft = document.querySelector(".projects-left");
+const projectsRight = document.querySelector(".projects-right");
+
+const projectS_COUNT = projects.length;
+let currentproject = 0;
+
+const updateproject = () => {
+  projectsCarousel.style.transform = `translateX(-${
+    currentproject * 100
+  }vw`;
+};
+
+projectsLeft.addEventListener("click", () => {
+  currentproject--;
+  if (currentproject < 0) {
+    currentproject = projectS_COUNT - 1;
+  }
+  updateproject();
+  console.log(currentproject);
+});
+
+projectsRight.addEventListener("click", () => {
+  currentproject++;
+  if (currentproject > projectS_COUNT - 1) {
+    currentproject = 0;
+  }
+  updateproject();
+  console.log(currentproject);
+});
